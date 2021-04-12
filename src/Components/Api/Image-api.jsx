21e603d126241ae34b2da27b47c1bae5cb41ff4f
Vita Khutorianska <steblvita@gmail.com>
2021-04-12 23:31:08 +0300

    fix apiimage

--- src/Components/Api/Image-api.jsx
+++ src/Components/Api/Image-api.jsx
@@ -1,10 +1,10 @@
-import axios from "axios";
-const ApiKey = "20342526-89d38198e8299a53d4950dffe";
-const fetchImages = ({ searchImage = "", currentPage = 1 }) => {
-  return axios
-    .get(
-      `https://pixabay.com/api/?q=${searchImage}&page=${currentPage}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=12`
-    )
-    .then((response) => response.data.hits);
+import axios from 'axios';
+const ApiKey = '20342526-89d38198e8299a53d4950dffe';
+const fetchImages = async ({ searchImage = '', currentPage = 1 }) => {
+  const response = await axios.get(
+    `https://pixabay.com/api/?q=${searchImage}&page=${currentPage}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=12`,
+  );
+  return response.data.hits;
 };
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { fetchImages };
